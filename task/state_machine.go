571a7b74881bea4d4a89654f549c3474d0717d96
oriiyx <paravinja92@gmail.com>
2025-04-10 21:08:05 +0200

    chapter 4

--- task/state_machine.go
+++ task/state_machine.go
@@ -10,60 +10,9 @@ const (
 	Failed
 )
 
-// type StateMachine struct {
-//	StartState   *State
-//	CurrentState *State
-// }
-//
-// func (s *StateMachine) GetCurrentState() State {
-//	return *s.CurrentState
-// }
-//
-// func (s *StateMachine) SetPending() error {
-//	if s.CurrentState == nil {
-//		s.CurrentState = *Pending
-//	}
-// }
-//
-// func (s *StateMachine) SetScheduled(currentState State) error {
-//	if currentState == Pending {
-//		s.CurrentState = Scheduled
-//		return nil
-//	}
-//
-//	return fmt.Errorf("cannot transition from %s to Scheduled", currentState)
-// }
-//
-// func (s *StateMachine) SetRunning(currentState State) error {
-//	if Contains(stateTransitionMap[currentState], Running) {
-//		s.CurrentState = Running
-//		return nil
-//	}
-//
-//	return fmt.Errorf("cannot transition from %s to Running", currentState)
-// }
-//
-// func (s *StateMachine) SetCompleted(currentState State) error {
-//	if Contains(stateTransitionMap[currentState], Completed) {
-//		s.CurrentState = Completed
-//		return nil
-//	}
-//
-//	return fmt.Errorf("cannot transition from %s to Completed", currentState)
-// }
-//
-// func (s *StateMachine) SetFailed(currentState State) error {
-//	if Contains(stateTransitionMap[currentState], Completed) {
-//		s.CurrentState = Failed
-//		return nil
-//	}
-//
-//	return fmt.Errorf("cannot transition from %s to Failed", currentState)
-// }
-
 var stateTransitionMap = map[State][]State{
 	Pending:   []State{Scheduled},
-	Scheduled: []State{Running, Failed},
+	Scheduled: []State{Scheduled, Running, Failed},
 	Running:   []State{Running, Completed, Failed},
 	Completed: []State{},
 	Failed:    []State{},
@@ -76,4 +25,8 @@ func Contains(states []State, state State) bool {
 		}
 	}
 	return false
+}
+
+func ValidStateTransition(src State, dst State) bool {
+	return Contains(stateTransitionMap[src], dst)
 }
